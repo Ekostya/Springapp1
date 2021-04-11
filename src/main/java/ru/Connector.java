d1cf92b6981e557366d4efc73059d9b0b0c75e13
Kostya <kostyaelozo@gmail.com>
2021-04-11 18:47:29 +0300

    add app

--- src/main/java/ru/Connector.java
+++ src/main/java/ru/Connector.java
@@ -2,20 +2,30 @@ package ru;
 
 import com.mysql.cj.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class Connector {
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName(Driver.class.getName());
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false","root","mk1050642");
-        Statement statement =connection.createStatement();
-        statement.execute("SELECT * FROM user");
-        statement.close();
-        connection.close();
+        String query = "select * from user";
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?useSSL=false", "root", "mk1050642");
+        Statement statement = connection.createStatement();
+        ResultSet resultSet=statement.executeQuery(query);
+        while (resultSet.next()) {
+            int id = resultSet.getInt(1);
+            String name = resultSet.getString(2);
+            String password = resultSet.getString(3);
+            String email=resultSet.getString(4);
+            System.out.printf("id: %d, name: %s, password: %s %n,email %s %n", id, name,password,email);
+        }
+
+
+            statement.execute("select * from user");
+            statement.close();
+            connection.close();
+        }
     }
-}
