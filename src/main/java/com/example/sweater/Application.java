eb3e3b299e6154063593ffc3ccb5e8b500d03d3f
Kostya <kostyaelozo@gmail.com>
2021-04-15 20:12:36 +0300

    add app

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -1,4 +1,4 @@
-package com.example.sweeater;
+package com.example.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,5 +7,4 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-}
+    }}
